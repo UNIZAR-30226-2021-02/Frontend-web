a3592c361f4872355a947876cd49380c02df2381
PabloDominguez00 <p.nogueras2000@gmail.com>
2021-03-22 10:42:18 +0100

    to no lo quiero pa na Please enter the commit message for your changes. Lines starting

--- pictyo-phone/src/App.vue
+++ pictyo-phone/src/App.vue
@@ -1,6 +1,7 @@
-<!--template>
+<template>
   <div id="app">
     <div id="nav">
+      
     </div>
     <router-view/>
   </div>
@@ -30,50 +31,4 @@
 #nav a.router-link-exact-active {
   color: #42b983;
 }
-</style-->
-
-<template>
-  <div id="app">
-    <!--img alt="Vue logo" src="./assets/logo.png"-->
-    <!--HelloWorld/-->
-    <signin/>
-    <!--button @click="goTo('./components/HelloWorld.vue')">Main Page</button>
-    <button @click="goTo('./components/singUp.vue')">Register</button>
-    <app-router></app-router-->
-  </div>
-</template>
-
-<script>
-//import HelloWorld from './components/HelloWorld.vue'
-import Signin from './components/SignIn.vue';
-//import AppRouter from './components/Router.vue'
-//import singUp from './components/singUp.vue'
-
-export default {
-  name: 'App',
-  components: {
-    //HelloWorld,
-    Signin,
-    //AppRouter,
-    //singUp
-  },
-  methods: {
-    goTo(route){
-      window.location=route;
-    }
-  }
-};
-
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
-
