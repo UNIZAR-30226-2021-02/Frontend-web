b410ce9cc1be7ef4950b46a774b5d5d22430783c
PabloDominguez00 <p.nogueras2000@gmail.com>
2021-04-28 17:39:11 +0200

    Cambio para intentar subir a ghbpages

--- pictyo-phone/vue.config.js
+++ pictyo-phone/vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-    ? '/UNIZAR-30226-2021-02/Frontend-web/' //nombre de tu proyecto GitHub
+    ? '/Frontend-web/' //nombre de tu proyecto GitHub 
     : '/'
+    
 }
