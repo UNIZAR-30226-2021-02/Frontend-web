16b532699e2e0491ad5e95e9de250aabdb895008
PabloDominguez00 <p.nogueras2000@gmail.com>
2021-05-16 15:01:00 +0200

    Marco realizado para la lista de amigos

--- pictyo-phone/src/views/FriendsView.vue
+++ pictyo-phone/src/views/FriendsView.vue
@@ -1,4 +1,4 @@
-<template>
+<!--template>
   <div class="FriendsView">
       <deshecho-component msg="Esto es la página de amigos"/>
       <navegation-menu-component/>
@@ -24,4 +24,72 @@ export default {
     FriendListsComponent
   }
 }
+</script-->
+
+<template>
+  <div class="HomeView">
+    <div class="sidenav">
+      <navegation-menu-component/>
+    </div>
+    <div class="content">
+      <friend-searcher-component/>
+      <friend-lists-component/>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import NavegationMenuComponent from '@/components/NavegationMenuComponent.vue'
+import friendSearcherComponent from '../components/friendSearcherComponent.vue'
+import FriendListsComponent from '../components/friendListsComponent.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    NavegationMenuComponent,
+    friendSearcherComponent,
+    FriendListsComponent
+  }
+}
 </script>
+
+<style scoped>
+  @media screen {
+    html, body{
+      background-color: grey;
+      position: absolute;
+      height: 100%;
+      width: 100%;
+    }
+
+    /* Style the side navigation */
+    .sidenav {
+      float: left;
+      background-color: white;
+      border-radius: 20px;
+      margin-top: 5%;
+      margin-left: 3%;
+      border: 5px solid blueviolet;
+    }
+
+    /* Style the content */
+    .content {
+      display: inline-block;
+      margin-top: 3%;
+    }
+
+    .HomeView{
+      border: 5px solid rgb(49, 49, 49);
+      position: absolute;
+      margin-left:10%;
+      margin-right: 10%;
+      margin-top: 5%;
+      background-color: #17151c ;
+      border-radius: 25px;
+      width: 80%;
+      height: 80%;
+    }
+  }
+</style>
+    
